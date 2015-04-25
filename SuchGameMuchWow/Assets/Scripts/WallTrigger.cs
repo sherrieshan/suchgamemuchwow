bb645de76dd92e5632fc212099b18c38904aff38
Sherrie Shan <sherrie.the.shan@gmail.com>
2015-04-25 16:56:42 -0700

    wall triggers

---
+++ SuchGameMuchWow/Assets/Scripts/WallTrigger.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WallTrigger : MonoBehaviour {
+	GameObject dogeTrig;
+	float posX;
+
+	// Use this for initialization
+	void Start () {
+		dogeTrig = GameObject.FindGameObjectWithTag ("Player");
+		posX = dogeTrig.transform.position.x;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.tag == "Player")
+		{
+			float y = other.GetComponent<Transform>().position.y;
+			other.GetComponent<Transform>().position = new Vector2(posX, y);
+		}
+	}
+}
