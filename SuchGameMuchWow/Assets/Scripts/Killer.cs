afac19194a0503455f72c63d566824c9c710508e
Jonathan Nguyen <jnguyen1123@gmail.com>
2015-04-25 21:25:17 -0700

    KILLER2

--- SuchGameMuchWow/Assets/Scripts/Killer.cs
+++ SuchGameMuchWow/Assets/Scripts/Killer.cs
@@ -3,13 +3,15 @@ using System.Collections;
 
 public class Killer : MonoBehaviour {
 	
-	public Vector3 originalPosition;
+	public Vector2 originalPosition;
+	public DogeController doge;
 	
 	// Use this for initialization
 	void Start () 
 	{
 		originalPosition = transform.position;
-		GetComponent<Rigidbody>().velocity = new Vector3(1,0,0);
+		GetComponent<Rigidbody2D>().velocity = new Vector2(1,0);
+		doge = GameObject.FindGameObjectWithTag("Player").GetComponent<DogeController>();
 	}
 	
 	// Update is called once per frame
@@ -20,5 +22,18 @@ public class Killer : MonoBehaviour {
 	public void reset()
 	{
 		transform.position = originalPosition;
+	}
+	
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if(other.tag == "coin")
+		{
+			doge.GetComponent<Health>().takeDamage(3);
+			Destroy(other.gameObject);
+		}
+		else if(other.tag == "Player")
+		{
+			doge.GetComponent<Health>().takeDamage(999999999);
+		}
 	}
 }
